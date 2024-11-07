34cb84a3ca7a66492493cf4bae691f9a43ef3ee0
ahmed-abdelhamid <slevin.glevra@gmail.com>
2024-11-07 09:42:19 +0300

    feat: activity view

--- src/app/[locale]/(dashboard)/activities/[id]/page.tsx
+++ src/app/[locale]/(dashboard)/activities/[id]/page.tsx
@@ -1,13 +1,14 @@
 import { setRequestLocale } from "next-intl/server"
 
 import { LocalizedPageProps } from "@/types"
-import { ResourceForm } from "@/features/resources/components/resources/resource-form"
 import { getResourceCategories } from "@/features/resources/lib/get-resource-categories"
 import { getResourceSubcategories } from "@/features/resources/lib/get-resource-subcategory"
-import { getCurrencies } from "@/features/currencies/lib/get-currencies"
 import { getUnits } from "@/features/units/lib/get-units"
 import { getResources } from "@/features/resources/lib/get-resources"
-import { getResourceById } from "@/features/resources/lib/get-resource-by-id"
+import { getActivityById } from "@/features/activities/lib/get-activity-by-id"
+import { getActivityCategories } from "@/features/activities/lib/get-activity-categories"
+import { getActivitySubcategories } from "@/features/activities/lib/get-activity-subcategory"
+import { ActivityForm } from "@/features/activities/components/activities/activity-form"
 
 interface CreateNewResourcePageProps extends LocalizedPageProps {
   params: { id: string; locale: "en" | "ar" }
@@ -18,25 +19,31 @@ const CreateNewResourcePage: React.FC<CreateNewResourcePageProps> = async ({
 }) => {
   setRequestLocale(locale)
 
-  const { resource } = await getResourceById(id)
-  const { categoriesOptions } = await getResourceCategories()
-  const { subcategoriesOptions } = await getResourceSubcategories()
-  const { currencies } = await getCurrencies()
+  const { activity } = await getActivityById(id)
+  const { categoriesOptions: activityCategoriesOptions } =
+    await getActivityCategories()
+  const { subcategoriesOptions: activitySubcategoriesOptions } =
+    await getActivitySubcategories()
+  const { categoriesOptions: resourceCategoriesOptions } =
+    await getResourceCategories()
+  const { subcategoriesOptions: resourceSubcategoriesOptions } =
+    await getResourceSubcategories()
   const { unitsOptions } = await getUnits()
   const { resources } = await getResources()
 
   return (
     <>
       <div className="flex flex-row items-center justify-between space-y-0 pb-4">
-        <h1 className="text-2xl font-bold">{resource.description}</h1>
+        <h1 className="text-2xl font-bold">{activity.description}</h1>
       </div>
-      <ResourceForm
-        resource={resource}
+      <ActivityForm
+        activity={activity}
         resources={resources}
-        categories={categoriesOptions}
-        subcategories={subcategoriesOptions}
-        currencies={currencies}
-        units={unitsOptions}
+        activityCategoriesOptions={activityCategoriesOptions}
+        activitySubcategoriesOptions={activitySubcategoriesOptions}
+        resourceCategoriesOptions={resourceCategoriesOptions}
+        resourceSubcategoriesOptions={resourceSubcategoriesOptions}
+        unitsOptions={unitsOptions}
       />
     </>
   )
